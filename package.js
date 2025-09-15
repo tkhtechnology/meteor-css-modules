5551896e6bdd2845160a4ea32ab9be35d90cf4bf
Robert Ratajczak <r.ratajczak@tkhtechnology.com>
2025-09-15 10:16:38 +0200

    chore: Release 7.0.10

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* globals Package */
 Package.describe({
     name: 'cctech:scss-modules',
-    version: '7.0.10-rc.0',
+    version: '7.0.10',
     summary: 'CSS modules implementation. CSS for components!',
     git: 'https://github.com/tkhtechnology/meteor-css-modules',
     documentation: 'README.md'
